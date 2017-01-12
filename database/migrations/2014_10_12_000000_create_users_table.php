045f93914cee0a1b05562b3133ea7bb4c6f1a2dd
Joël De Sousa <joel.DE-SOUSA@cpnv.ch>
2017-01-12 14:16:47 +0100

    add_links migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -25,10 +25,6 @@ class CreateUsersTable extends Migration
             $table->text('qr_code')->unique();
             $table->rememberToken();
             $table->timestamps();
-            $table->integer('role_id')->unsigned();
-            $table->foreign('role_id')->references('id')->on('roles');
-            $table->integer('group_id')->unsigned();
-            $table->foreign('group_id')->references('id')->on('groups');
         });
     }
 
---
+++ database/migrations/2017_01_12_130336_add_links.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddLinks extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      // create_users
+      Schema::table('users', function (Blueprint $table) {
+        $table->integer('role_id')->unsigned();
+        $table->foreign('role_id')->references('id')->on('roles');
+        $table->integer('group_id')->unsigned();
+        $table->foreign('group_id')->references('id')->on('groups');
+      });
+      //runs
+      Schema::table('runs', function (Blueprint $table) {
+        $table->integer('user_id')->unsigned();
+        $table->foreign('user_id')->references('id')->on('users');
+      });
+
+      //cars
+      Schema::table('cars', function (Blueprint $table) {
+        $table->integer('car_types_id')->unsigned();
+        $table->foreign('car_types_id')->references('id')->on('car_types');
+
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+      // create_users
+      Schema::table('users', function (Blueprint $table) {
+        $table->dropForeign(['role_id','group_id']);
+        $table->dropColumn(['role_id', 'group_id']);
+      });
+      //runs
+      Schema::table('runs', function (Blueprint $table) {
+        $table->dropForeign(['user_id']);
+        $table->dropColumn(['user_id']);
+      });
+
+      //cars
+      Schema::table('cars', function (Blueprint $table) {
+        $table->dropForeign(['car_types_id']);
+        $table->dropColumn(['car_types_id']);
+      });
+    }
+}
