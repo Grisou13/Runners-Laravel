ff8bcdae638f437c56634c4aba6b2dcf3de3c1e7
Thomas Ricci <thomas.ricci@cpnv.ch>
2017-01-26 09:38:29 +0100

    link errors fix

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -3,7 +3,7 @@
 @section("content")
 <nav class="navbar navbar-inverse">
     <ul class="nav navbar-nav">
-        <li><a href="{{ URL::to('user') }}">View All Users</a></li>
+        <li><a href="{{ route("users.index") }}">View All Users</a></li>
     </ul>
 </nav>
 @include("partials.user.edit")
