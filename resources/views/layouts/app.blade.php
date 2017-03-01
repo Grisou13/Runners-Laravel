3d6bebb152b6971e1fb73616664cedcce1cf674c
Thomas Ricci <thomas.ricci@cpnv.ch>
2017-03-01 14:18:33 +0100

    added stuff

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,8 @@
 
     <!-- Styles -->
     <link href="{{ asset("/css/app.css") }}" rel="stylesheet">
-    <link href="{{ asset("/css/theme.css") }}" rel="stylesheet">
+    <link href="{{ asset("/css/style.css") }}" rel="stylesheet">
+    {{--<link href="{{ asset("/css/theme.css") }}" rel="stylesheet">--}}
 
     @stack("styles")
 
