0156475cdcc98d829cfae89ab7755845ee59afb2
Eric <paralysant@gmail.com>
2017-03-15 14:54:17 +0100

    Added schedule basic CRUD
    ... with api

---
+++ api/Controllers/V1/SettingController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Api\V1\Controllers;
+
+use Api\Controllers\BaseController;
+use Lib\Models\Setting;
+use Illuminate\Http\Request;
+
+class SettingController extends BaseController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Setting::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return Setting::create($request->except(["token","_token"]));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Settings  $settings
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Setting $settings)
+    {
+        return $settings;
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Settings  $settings
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Setting $settings)
+    {
+        $settings->update($request->except(["token","_token"]));
+        return $settings;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Settings  $settings
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Setting $settings)
+    {
+        $settings->delete();
+    }
+}
---
+++ lib/Models/Setting.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Lib\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Setting extends Model
+{
+    public $timestamps = false;
+    public $fillable = ["key","value"];
+}
