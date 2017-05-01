d674f105c7bda23ef29a36226338e49c7e5f722c
Eric <eric.bousbaa@hotmail.com>
2017-05-01 15:48:45 +0200

    Kiéla basics done.
     Still need UI developpement

--- resources/views/kiela/index.blade.php
+++ resources/views/kiela/index.blade.php
@@ -3,11 +3,12 @@
 <link rel="stylesheet" href="{{ asset("/css/schedule.css") }}">
 @endpush
 @push("scripts")
+<script src="{{asset("js/underscore.min.js")}}"></script>
 <script src="{{ asset("/js/kiela.js") }}"></script>
 @endpush
 
 @section("content")
     <div class="kiela">
-        
+
     </div>
 @endsection
