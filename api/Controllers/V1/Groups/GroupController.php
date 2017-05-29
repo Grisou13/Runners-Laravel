8a75d9fd5a237034c35ff24f59c28abac0aa929e
Eric <eric.bousbaa@hotmail.com>
2017-05-29 16:50:06 +0200

    new group name bug fix

--- api/Controllers/V1/Groups/GroupController.php
+++ api/Controllers/V1/Groups/GroupController.php
@@ -50,7 +50,7 @@ class GroupController extends BaseController
         $group->color = Helpers\Helper::getRandomGroupColor();
         $group->save();
         $alphabet = Helpers\Helper::mkrange("A", "ZZ");
-        $group->name = $alphabet[$group->id - 1];
+        $group->name = $alphabet[Group::count() - 1];
         $group->save();
         return $group;
         return $this->response()->created(route("groups.show",$group->id));
