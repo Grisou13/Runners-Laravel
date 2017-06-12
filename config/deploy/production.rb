e9b1c7bad010024fd5804a33ff2fd3808bd60369
Thomas Ricci <thomas.ricci@cpnv.ch>
2017-06-12 10:21:28 +0200

    Auto stash before merge of "develop" and "origin/develop"

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,7 +4,7 @@
 # You can define all roles on a single server, or split them:
 
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
-# server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
+# snetstat -lerver "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 server "46.101.24.93", user: "deploy", roles: %w{app,db,cache}
 
 
