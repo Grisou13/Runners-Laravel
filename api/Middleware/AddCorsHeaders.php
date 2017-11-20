35c52da90b67df44aabff0b9445e89eb92702900
Thomas Ricci <grisou13@hotmail.fr>
2017-11-20 15:52:44 +0100

    fixed cors headers for ionic

--- api/Middleware/AddCorsHeaders.php
+++ api/Middleware/AddCorsHeaders.php
@@ -18,12 +18,13 @@ class AddCorsHeaders
      * @var $response \Illuminate\Http\Response
      */
     $response = $next($request);
-  
+
     // Perform action
     $response->withHeaders([
       "Access-Control-Allow-Origin"=>"*",
       "Access-Control-Allow-Headers" => "X-Requested-With, Content-Type, X-Access-Token, x-access-token, Authorization, api_key",
       "Access-Control-Request-Method"=>"GET, POST, PUT, PATCH, DELETE",
+      "Access-Control-Allow-Methods"=>"GET, POST, PUT, PATCH, DELETE",
       "Access-Control-Request-Headers"=>"X-Requested-With, Content-Type, X-Access-Token, x-access-token, Authorization, api_key",
       "Access-Control-Max-Age"=>0
     ]);
